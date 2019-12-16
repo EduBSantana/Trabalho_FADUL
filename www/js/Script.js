c3adf293d029de58907a1f0631a4d32eb0f9a93f
EduBSantana <edubarrossantana17@gmail.com>
2019-12-16 11:27:39 -0300

    Erros resolvidos, botao comprar funcionando

--- www/js/Script.js
+++ www/js/Script.js
@@ -22,8 +22,8 @@ function successDB() { }
 
 //Cria a tabela se a mesma não existir    
 function createDB(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS Cadastro (id INTEGER PRIMARY KEY, nome VARCHAR(50), qtd INTEGER, preco FLOAT )');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS Compras (id INTEGER PRIMARY KEY, nome_cadastro VARCHAR(50), qtd_compra INTEGER, total FLOAT )');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Cadastro (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome VARCHAR(50), qtd INTEGER, preco FLOAT )');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS compra (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, id_cad INTEGER, qtd_compra INTEGER, valor_total REAL NOT NULL, FOREIGN KEY(id_cad) REFERENCES Cadastro(id))');
 }
 
 //Exlusao
@@ -45,21 +45,16 @@ function Cadastro_delete_db(tx){
 
 // Fim exclusao
 
-// Prepara para incluir registro na tabela Agenda
+// Prepara para incluir registro na tabela Cadastro
 function Cadastro_insert() {
 	db.transaction(Cadastro_insert_db, errorDB, successDB);
 }
 
-function Compras_insert() {
-	db.transaction(Compras_insert_db, errorDB, successDB);
-}
 
-//Fazer as compras
-
-// Inclui registro na tabela Agenda
+// Inclui registro na tabela Cadastro
 function Cadastro_insert_db(tx) {
    
-
+    
 	var nome = $("#Cadastro_nome").val();
     var qtd = $("#Cadastro_qtd").val();
     var preco = $("#Cadastro_preco").val();
@@ -80,11 +75,6 @@ function cadastrado(){
     $("#Cadastro_preco").val("");
 }
 
-/*function regNumber() {
-
-    $("#headertemp").append("<h4>Número de registros </h4>" + cad);
-
-}*/
 
 //MONTA A MATRIZ COM OS REGISTRO DA TABELA AGENDA
 function Cadastro_view(){
@@ -116,45 +106,160 @@ function Cadastro_view_data(tx, results){
         "<td><h5>"+ results.rows.item(i).qtd + "</h5></td>"+
         "<td><h5> R$ "+ results.rows.item(i).preco + "</h5></td>"+
         "<td><button type='button' class='btn btn-danger' onclick='Cadastro_delete("+ results.rows.item(i).id+")'>Delete" +
-        " <button type='button' class='btn btn-success'  onclick='comprarMoto("+ results.rows.item(i).id+")'>Buy <img src='img/car.png' style='width: 25px'></td>" +
+        " <button type='button' class='btn btn-success'  onclick='tela_listar_mostrar_tela_compra("+ results.rows.item(i).id+")'>Buy <img src='img/car.png' style='width: 25px'></td>" +
         
         "</tr>");
     }
 
 }
 
-function dadosMoto_view(){
 
-    db.transaction(dadosMoto_view_db, errorDB, successDB); 
-
+//Prepara para incluir registro na tabela compra
+function compra_insert()
+{
+    
+	db.transaction(compra_insert_db, errorDB, successDB);
 }
 
-//monta a matriz com os registros da tabela agenda
-function dadosMoto_view_db(tx){
+//Inclui registro na tabela compra
+function compra_insert_db(tx)
+{
 
-    tx.executeSql('SELECT nome,preco FROM Cadastro',[], dadosMoto_view_data, errorDB);
+	var id_cad_compra = $("#id_cad_compra").val();
+	var qtd_compra = $("#qtd_compra").val();
+	var valor_unit_compra = $("#valor_unit_compra").val();
+
+	var qtd_estoque = 0;
+
+	var valor_total = (qtd_compra * valor_unit_compra);
+
+	tx.executeSql('SELECT * FROM Cadastro WHERE id = '+ id_cad_compra + '', [], function(tx,results) 
+	{
+		for (var i = 0; i < results.rows.length; i++)
+		{
+			qtd_estoque = results.rows.item(i).qtd_estoque;
+		}
+		if (qtd_estoque < qtd_compra) 
+		{
+
+			//Emite uma mensagem caso a compra exceda o estoque disponível
+			return alert('Estoque insuficiente!'); 
+		}
+		else
+		{
+			//Emite mensagem caso a compra seja sucedida
+			alert("Produto adicionado ao carrinho!");
+		}
+
+		//Insert na tabela de compras e update na tabela de produto pra diminuir o valor do estoque
+		tx.executeSql('INSERT INTO compra (id_cad, qtd_compra, valor_total) VALUES ("' + id_cad_compra + '", "' + qtd_compra+ '", "' + valor_total + '")');
+		tx.executeSql('UPDATE Cadastro SET qtd = (qtd - "' + qtd_compra + '") WHERE id = "' + id_cad_compra + '"');
+
+	});
+
+	//Atualiza o carrinho
+	compra_view();
+	Cadastro_view();
+
+
+	
 }
 
-function dadosMoto_view_data(dm, results) {
 
-    alert("a");
-    $("#dados").append("<h1>Modelo: " + results.rows.item(i).nome + "</h1>");
+//Prepara pra chamar a função de listar o carrinho
+function compra_view()
+{
+	db.transaction(compra_view_db,errorDB,successDB);
+}
 
+//Select do carrinho que mostra o ID do produto comprado (o id da tabela produto), valor do produto, nome e valor total da compra
+function compra_view_db(tx)
+{
+	tx.executeSql('SELECT t1.id_cad, t1.qtd_compra, t1.valor_total, t2.id, t2.nome FROM compra t1 INNER JOIN Cadastro t2 on (t1.id_cad = t2.id)',[],compra_view_data,errorDB);
+}
+
+//Lista o select na table
+function compra_view_data(tx,results)
+{
+	$("#compra_listagem").empty();
+	var len = results.rows.length;
+
+	//Variavel pra armazenar o valor total da compra
+	var valorCalculado = 0;
+
+	for(var i = 0; i <len;i++)
+	{
+	$("#compra_listagem").append("<tr class='compra_item_lista'>"+
+		"<td><h3>" + results.rows.item(i).id + "</h3></td>"+
+		"<td><h3>" + results.rows.item(i).nome + "</h3></td>"+
+		"<td><h3>" + results.rows.item(i).qtd_compra + "</h3></td>"+
+		"<td class='valor-calculado'><h3>" + results.rows.item(i).valor_total + "</h3></td>"
+		+ "</tr>");
+	}
+
+	//Função que atribui o valor da compra individual na compra total
+    $( ".valor-calculado" ).each(function() {
+      	valorCalculado += parseInt($( this ).text());
+    });
+
+	//Mostrando o valor total da compra no campo embaixo da listagem de carrinho
+	var totalcampo = document.getElementById('total');
+	totalcampo.value = valorCalculado;
+}
+
+function finalizar_compra()
+{
+	db.transaction(finalizar_compra_db,errorDB,successDB);
+}
+
+function finalizar_compra_db(tx)
+{
+    
+	tx.executeSql('DELETE FROM compra');
+	alert("Compra Finalizada!");
+    compra_view();
+	
 }
 
 
-function realizaCompra(cadastro_id) {
+function tela_listar_mostrar_tela_compra(_id)
+{
+	//Variáveis que guardam os dados do produto que a pessoa quer comprar
+	var id_cad_compra = document.getElementById('id_cad_compra');
+	var nome_compra = document.getElementById('nome_compra');
+    var valor_unit_compra = document.getElementById('valor_unit_compra');
+    var qtd_compra = document.getElementById('qtd_compra');
+    
+    id_cad_compra.value = _id;
+    
+    db.transaction(function(tx) {
+        tx.executeSql('SELECT * FROM Cadastro WHERE id=?', [_id], function (tx, resultado) {
+            var rows = resultado.rows[0];
 
+            //Atribui o valor das variáveis no campo pra que o usuário não se preocupe em digitar o ID do produto e o Valor individual do produto
+            id_cad_compra.value = rows.id;
+            nome_compra.value = rows.nome;
+            valor_unit_compra.value = rows.preco;
+            qtd_compra.value = '';
+        });
+    });
 
-
+    $("#TelaPadrao").hide();
+    $("#TelaListagem").hide();
+    $("#TelaCadastro").hide();
+    $("#tela_carrinho").hide();
+    $("#tela_comprar").show(800);
+	
 }
+
+
 
 function esconde() {
 
-    $("#TelaCompra").hide();
     $("#TelaCadastro").hide();
     $("#TelaListagem").hide();
-    $("#TelaCompra").hide();
+    $("#tela_carrinho").hide();
+    $("#tela_comprar").hide();
 
 }
 
@@ -162,7 +267,8 @@ function voltar() {
 
     $("#TelaCadastro").hide(800);
     $("#TelaListagem").hide(800);
-    $("#TelaCompra").hide(800);
+    $("#tela_carrinho").hide(800);
+    $("#tela_comprar").hide(800);
     $("#TelaPadrao").show(800);
 
 }
@@ -173,6 +279,8 @@ function cadastrarMoto() {
     $("#TelaCompra").hide();
     $("#TelaPadrao").hide();
     $("#TelaListagem").hide();
+    $("#tela_carrinho").hide();
+    $("#tela_comprar").hide();
     $("#TelaCadastro").show(800);
 
 }
@@ -182,59 +290,19 @@ function comprarMoto() {
     $("#TelaPadrao").hide();
     $("#TelaListagem").hide();
     $("#TelaCadastro").hide();
-    $("#TelaCompra").show(800);
-
-    dadosMoto_view()
+    $("#tela_carrinho").show(800);
+    $("#tela_comprar").hide();
 
 }
-
 
 //
 function listarMoto() {
 
     $("#TelaPadrao").hide();
     $("#TelaCadastro").hide();
+    $("#tela_carrinho").hide(800);
+    $("#tela_comprar").hide(800);
     $("#TelaListagem").show(800);
 
 }
 
-
-/*function Cadastro_update_abrir_tela(agenda_id) {
-
-    $("#tela_padrao").hide(); //Esconde Tela inicial
-    $("#tela_edicao").show(800); //Mostra tela de Edição
-
-    var agenda_nome_update = $("#agenda_item_" + agenda_id + " .agenda_info h3").html();
-    var agenda_telefone_update = $("#agenda_item_" + agenda_id + " .agenda_info h5").html();
-
-    $("#agenda_id_update").val(agenda_id);
-    $("agenda_nome_update").val(agenda_nome_update);
-    $("#agenda_telefone_update").val(agenda_telefone_update);
-
-}
-
-function agenda_update_fechar_tela() {
-
-    $("#tela_edicao").hide(800); //Esconde tela de edição
-    $("#tela_padrao").show(); //Mostra tela inical
-
-}
-
-function agenda_update() {
-
-    db.transaction(agenda_update_db, errorDB, successDB);
-
-}
-
-function agenda_update_db(tx) {
-
-    var agenda_id_novo = $("#agenda_id_update").val();
-    var agenda_nome_novo= $("#agenda_nome_update").val();
-    var agenda_telefone_novo = $("#agenda_telefone_update").val();
-
-    tx.executeSql('UPDATE agenda SET nome = "' + agenda_nome_novo + '", tel ="' + agenda_telefone_novo + '" WHERE id= "' + agenda_id_novo + '" ')
-
-    agenda_update_fechar_tela();
-    agenda_view();
-
-}*/
